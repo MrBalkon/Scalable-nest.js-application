9a89e0aff6f349cbccb132bd7b4389412cfe25c2
Evgeny <jen31111@gmail.com>
2021-02-22 19:23:58 +0300

    Hotfix: fix names

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,4 @@
-import { NewsModule } from '@application/http/news/news.module'
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
-import { CardModule } from '@application/http/card/card.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configDBService } from './infrastructure/db/db.service'
@@ -8,7 +6,7 @@ import { LoggerMiddleware } from '@infrastructure/logger/logger.middleware';
 import { AuthCheckerMiddleware } from '@application/http/shared/auth/auth.middleware'
 
 @Module({
-  imports: [NewsModule, CardModule, ConfigModule.forRoot(), TypeOrmModule.forRoot(configDBService.getTypeOrmConfig())],
+  imports: [ ConfigModule.forRoot(), TypeOrmModule.forRoot(configDBService.getTypeOrmConfig())],
   controllers: [],
   providers: [],
 })
